094e7bc6c936ddf8bc4b972dad4830490d9f84a5
ljy0812 <ljy0812@midasit.com>
2019-07-30 17:51:21 +0900

    feat(#4) : Create Vector Template
    create VectorTemplate.h using template function
    
    add test code in main.cpp
    add comment to VectorManager Testcode in main.cpp

---
+++ cppStudy/VectorTemplate.h
@@ -0,0 +1,231 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <map>
+#include <assert.h>
+
+namespace VectorSpace {
+
+	template <class Type>
+	class Vector {
+	private:
+		//중복체크
+		//decltype(auto) 정확한 데이터타입을 알아낼 수 있음		
+		std::optional<int> IsDuplicateDataInVectorReturnIdx(Type data);
+		//삭제 후 데이터 재정렬
+		void ShiftLeft(int index);
+
+		int m_vectorId;
+		int m_vectorSize;
+		Type *m_pData;
+
+	public:
+		Vector();
+		virtual ~Vector() 
+		{ 
+			// Safe Delete
+			if (m_pData)
+				delete[]m_pData;
+			std::cout << "Delete Vector" << std::endl;
+		};
+
+		//데이터 추가		
+		bool AddData(Type data);
+
+		//데이터 삭제		
+		bool DeleteData(Type data);
+
+		//현재 벡터의 데이터 출력
+		//cf) alt+shift를 이용하면 세로로 편집가능 :)		
+		void PrintData();
+		//현재 데이터 개수 반환		
+		int GetSize() { return m_vectorSize; }
+
+		void SetVectorId(int autoId) { m_vectorId = autoId; }
+		const int GetVectorId() const{ return m_vectorId; }
+
+	};
+	template <class Type>
+	class VectorManager
+	{
+	private:
+		std::map<int, std::shared_ptr<Vector<Type>>> m_id2Vector{};
+		int autoIncreasementId;// Vector Id
+
+	public:
+		VectorManager();
+		virtual ~VectorManager() { std::cout << "Delete VectorManager" << std::endl; };
+
+		//벡터 생성
+		void CreateVector(std::shared_ptr<Vector<Type>> newVector);
+		//생성된 백터에 데이터 추가
+		bool Append(int varId, Type data);
+		//모든 자료구조 정보 출력
+		void PrintInformation();
+		//벡터 내에 있는 데이터 삭제
+		bool DeleteDataInVector(int varId, Type data);
+		//벡터 삭제
+		bool DeleteVector(int varId);			   		
+	
+	};
+
+	//Vector Class 함수 구현
+	template <class Type>
+	std::optional<int> Vector<Type>::IsDuplicateDataInVectorReturnIdx(Type data) {
+
+		for (int i = 0; i < m_vectorSize; i++)
+		{
+			if (m_pData[i] == data)
+			{
+				return i;
+			}
+		}
+
+		return {};
+
+	}
+
+	template <class Type>
+	void Vector<Type>::ShiftLeft(int index) 
+	{
+		for (int i = index; i < m_vectorSize; i++)
+		{
+			m_pData[i] = m_pData[i + 1];
+		}
+	}
+
+	template <class Type>
+	Vector<Type>::Vector() 
+	{
+		m_vectorId = 0;
+		m_vectorSize = 0;
+
+		m_pData = new Type[30];
+	}
+
+	template <class Type>
+	bool Vector<Type>::AddData(Type data) 
+	{
+		if (const auto& index = IsDuplicateDataInVectorReturnIdx(data); !index) {
+			m_pData[m_vectorSize] = data;
+			m_vectorSize++;
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	template <class Type>
+	bool Vector<Type>::DeleteData(Type data) 
+	{
+		if (const auto& index = IsDuplicateDataInVectorReturnIdx(data); index)
+		{
+			m_vectorSize--;
+			ShiftLeft(*index);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	template <class Type>
+	void Vector<Type>::PrintData() 
+	{
+		std::cout << "VectorData : ";
+
+
+		for (int i = 0; i < m_vectorSize; i++)
+		{
+			std::cout << m_pData[i] << "  ";
+		}
+
+		std::cout << "\n";
+		std::cout << "VectorSize : " << m_vectorSize << "\n";
+
+	}
+	   
+	//VectorManager Class 함수 구현
+	template <class Type>
+	VectorManager<Type>::VectorManager() 
+	{
+		autoIncreasementId = 0;
+	}
+
+	template <class Type>
+	void VectorManager<Type>::CreateVector(std::shared_ptr<Vector<Type>> newVector) 
+	{
+		newVector->SetVectorId(++autoIncreasementId);
+		m_id2Vector.emplace(autoIncreasementId, newVector);
+	}
+
+	template <class Type>
+	bool VectorManager<Type>::Append(int varId, Type data)
+	{
+		if (varId <= 0)
+			assert(0);
+
+		if (m_id2Vector.find(varId) != m_id2Vector.end()) 
+		{
+			return m_id2Vector[varId]->AddData(data);
+		}			
+		else 
+		{
+			return false;
+		}
+	}
+
+	template <class Type>
+	void VectorManager<Type>::PrintInformation() 
+	{
+		for (const auto& element : m_id2Vector) 
+		{
+			std::cout << "Vector (autoIncreasementId : " << element.second->GetVectorId() << ") " << "\n";
+			element.second->PrintData();
+		}
+		
+		std::cout << "---------------------------------------" << "\n";
+	}
+
+	template <class Type>
+	bool VectorManager<Type>::DeleteDataInVector(int varId, Type data) 
+	{
+		return m_id2Vector[varId]->DeleteData(data);
+	}
+
+	template <class Type>
+	bool VectorManager<Type>::DeleteVector(int varId) 
+	{
+		if (varId <= 0)
+			return false;
+
+		if (m_id2Vector.find(varId) == m_id2Vector.end())
+		{
+			return false;
+		}
+		else
+		{
+			m_id2Vector.erase(varId);
+			return true;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	
+
+
+}
+
--- cppStudy/main.cpp
+++ cppStudy/main.cpp
@@ -1,5 +1,6 @@
 
-#include "VectorManager.h"
+//#include "VectorManager.h"
+#include "VectorTemplate.h"
 #include <string>
 
 void PrintMessage(std::string msg)
@@ -9,74 +10,173 @@ void PrintMessage(std::string msg)
 
 int main() 
 {
-	std::unique_ptr<VectorSpace::VectorManager> vectorManager = std::make_unique<VectorSpace::VectorManager>();
-
-	// Initial 3 Vector
+	//Vector Template Test Code
 	{
-		vectorManager->PrintInformation();
-		vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
-		vectorManager->PrintInformation();
-		vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
-		vectorManager->PrintInformation();
-		vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
-		vectorManager->PrintInformation();
-	}
+		std::unique_ptr<VectorSpace::VectorManager<int>> vectorTemplate = std::make_unique<VectorSpace::VectorManager<int>>();
+		std::unique_ptr<VectorSpace::VectorManager<double>> vectorTemplate2 = std::make_unique<VectorSpace::VectorManager<double>>();
 
-
-	// Invalid
-	{
-		if (auto check = vectorManager->Append(4, 5); !check)
-			PrintMessage("No ID");
+		//Initial 3 integer Vector
+		{
+			vectorTemplate->PrintInformation();
+			vectorTemplate->CreateVector(std::make_shared<VectorSpace::Vector<int>>());
+			vectorTemplate->PrintInformation();
+			vectorTemplate->CreateVector(std::make_shared<VectorSpace::Vector<int>>());
+			vectorTemplate->PrintInformation();
+			vectorTemplate->CreateVector(std::make_shared<VectorSpace::Vector<int>>());
+			vectorTemplate->PrintInformation();
+		}
 		
-		// vectorManager->Append(0, 5); // Invalid ID는 0초과 assert
+		// Invalid
+		{
+			if (auto check = vectorTemplate->Append(4, 5); !check)
+				PrintMessage("No ID");
+
+			// vectorManager->Append(0, 5); // Invalid ID는 0초과 assert
+		}
+
+		// ID : 1 Append Integer Vector
+		{
+			vectorTemplate->Append(1, 5);
+			vectorTemplate->Append(1, 7);
+			vectorTemplate->Append(1, 10);
+			vectorTemplate->PrintInformation();
+		}
+		
+
+		// ID : 2 Append Integer Vector
+		{
+			vectorTemplate->Append(2, 5);
+			vectorTemplate->PrintInformation();
+		}
+
+		// ID : 3 Append Integer Vector
+		{
+			vectorTemplate->Append(3, 555);
+			vectorTemplate->PrintInformation();
+		}
+
+		
+
+		// Delete Data In Vector
+		{
+			if (auto check = vectorTemplate->DeleteDataInVector(3, 5); !check)
+				PrintMessage("No Data");
+			vectorTemplate->PrintInformation();
+
+			if (auto check = vectorTemplate->DeleteDataInVector(3, 555); check)
+				PrintMessage("Delete Data Success");
+			vectorTemplate->PrintInformation();
+		}
+
+		// Delete Vector
+		{
+			if (auto check = vectorTemplate->DeleteVector(5); !check)
+				PrintMessage("Delete Vector Fail");
+
+			if (auto check = vectorTemplate->DeleteVector(1); check)
+				PrintMessage("Delete Vector Success");
+			if (auto check = vectorTemplate->DeleteVector(2); check)
+				PrintMessage("Delete Vector Success");
+			if (auto check = vectorTemplate->DeleteVector(3); check)
+				PrintMessage("Delete Vector Success");
+			vectorTemplate->PrintInformation();
+		}
+
+		//Initial 3 Double Vector
+		{
+			PrintMessage("----------Apeend Double Data--------------");
+			vectorTemplate2->PrintInformation();
+			vectorTemplate2->CreateVector(std::make_shared<VectorSpace::Vector<double>>());
+			vectorTemplate2->PrintInformation();
+			vectorTemplate2->CreateVector(std::make_shared<VectorSpace::Vector<double>>());
+			vectorTemplate2->PrintInformation();
+			vectorTemplate2->CreateVector(std::make_shared<VectorSpace::Vector<double>>());
+			vectorTemplate2->PrintInformation();
+		}
+		// ID : 4 Append Double Vector
+		{
+			vectorTemplate2->Append(1, 1.5);
+			vectorTemplate2->Append(1, 7.4);
+			vectorTemplate2->Append(1, 10.234);
+			vectorTemplate2->PrintInformation();
+		}
 	}
 
 
-	// ID : 1 Append
+	/*
+	//Vector,List DataManager Test Code
 	{
-		vectorManager->Append(1, 5);
-		vectorManager->Append(1, 7);
-		vectorManager->Append(1, 10);
-		vectorManager->PrintInformation();
+		std::unique_ptr<VectorSpace::VectorManager> vectorManager = std::make_unique<VectorSpace::VectorManager>();
+
+		// Initial 3 Vector
+		{
+			vectorManager->PrintInformation();
+			vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
+			vectorManager->PrintInformation();
+			vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
+			vectorManager->PrintInformation();
+			vectorManager->CreateVector(std::make_shared<VectorSpace::Vector>());
+			vectorManager->PrintInformation();
+		}
+
+
+		// Invalid
+		{
+			if (auto check = vectorManager->Append(4, 5); !check)
+				PrintMessage("No ID");
+
+			// vectorManager->Append(0, 5); // Invalid ID는 0초과 assert
+		}
+
+
+		// ID : 1 Append
+		{
+			vectorManager->Append(1, 5);
+			vectorManager->Append(1, 7);
+			vectorManager->Append(1, 10);
+			vectorManager->PrintInformation();
+		}
+
+		// ID : 2 Append
+		{
+			vectorManager->Append(2, 5);
+			vectorManager->PrintInformation();
+		}
+
+		// ID : 3 Append
+		{
+			vectorManager->Append(3, 555);
+			vectorManager->PrintInformation();
+		}
+
+
+		// Delete Data In Vector
+		{
+			if (auto check = vectorManager->DeleteDataInVector(3, 5); !check)
+				PrintMessage("No Data");
+			vectorManager->PrintInformation();
+
+			if (auto check = vectorManager->DeleteDataInVector(3, 555); check)
+				PrintMessage("Delete Data Success");
+			vectorManager->PrintInformation();
+		}
+
+		// Delete Vector
+		{
+			if (auto check = vectorManager->DeleteVector(5); !check)
+				PrintMessage("Delete Vector Fail");
+
+			if (auto check = vectorManager->DeleteVector(1); check)
+				PrintMessage("Delete Vector Success");
+			if (auto check = vectorManager->DeleteVector(2); check)
+				PrintMessage("Delete Vector Success");
+			if (auto check = vectorManager->DeleteVector(3); check)
+				PrintMessage("Delete Vector Success");
+			vectorManager->PrintInformation();
+		}
+
 	}
-
-	// ID : 2 Append
-	{
-		vectorManager->Append(2, 5);
-		vectorManager->PrintInformation();
-	}
-
-	// ID : 3 Append
-	{
-		vectorManager->Append(3, 555);
-		vectorManager->PrintInformation();
-	}
-	
-
-	// Delete Data In Vector
-	{
-		if (auto check = vectorManager->DeleteDataInVector(3, 5); !check)
-			PrintMessage("No Data");
-		vectorManager->PrintInformation();
-
-		if (auto check = vectorManager->DeleteDataInVector(3, 555); check)
-			PrintMessage("Delete Data Success");
-		vectorManager->PrintInformation();
-	}
-
-	// Delete Vector
-	{
-		if (auto check = vectorManager->DeleteVector(5); !check)
-			PrintMessage("Delete Vector Fail");
-
-		if (auto check = vectorManager->DeleteVector(1); check)
-			PrintMessage("Delete Vector Success");
-		if (auto check = vectorManager->DeleteVector(2); check)
-			PrintMessage("Delete Vector Success");
-		if (auto check = vectorManager->DeleteVector(3); check)
-			PrintMessage("Delete Vector Success");
-		vectorManager->PrintInformation();
-	}
+	*/
 
 	// DataManager 예제
 	{
